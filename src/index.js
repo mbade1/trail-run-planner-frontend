651bcaea9a90414ff2437153680be641bf360050
mbade1 <mbade314@gmail.com>
2020-09-22 22:14:42 -0500

    store created

--- src/index.js
+++ src/index.js
@@ -4,14 +4,24 @@ import './index.css';
 import App from './App';
 
 //new store
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 
+
+//setup the store
+//applyMiddleware
+//pass in the reducer - hasn't been created yet. Need to create this! Second arg is any middleware, in this case, thunk
+//Also need to set up dev tools
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store = {store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
