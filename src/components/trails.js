31d0b8b9263db6fa19415128e06d1181c4ca82cc
mbade1 <mbade314@gmail.com>
2020-10-10 20:37:47 -0500

    Trails uses logic to render form for adding runs to journal

--- src/components/trails.js
+++ src/components/trails.js
@@ -21,18 +21,12 @@ class Trails extends React.Component {
         })
     }
 
-    // - Posting trail into Journal; plan what is needed (trail ID, user ID)
-    // - fetch request: `/users/:user_id/journals`                                                     
-    // - journal controller: #create with params of the dateOfRun and hikerProjectId???
-
     handleSubmit = event => {
         event.preventDefault()
         //function call to post into user's journal: PostRunToJournal(this.state.dateOfRun)
         //optional: this.props.history.push('/users/:id/journals)
         console.log(event)
         let userId = window.localStorage['id']
-
-        
         let dateOfRun = this.state.dateOfRun
         // JournalPost(userId, this.state.userId, this.state.dateOfRun)
         let hikerProjectId = event.target[1].defaultValue
@@ -73,13 +67,14 @@ class Trails extends React.Component {
             })
             .then(response => response.json())
             .then(newJournalEntry => console.log(newJournalEntry))
-
         this.setState({
             dateOfRun: ''
         })
+        alert('This run has been added to your journal!')
     }
 
     render() {
+      if (window.localStorage['id']) {
         return (
             <div className="trails">
                 {this.props.trails.map(trail => 
@@ -123,6 +118,36 @@ class Trails extends React.Component {
                 )}
             </div>
           )
+        }
+        else {
+          return (
+            <div className="trails">
+                {this.props.trails.map(trail => 
+                <div key={trail.id} className='trail'>
+                    <img src={trail.imgMedium || ImageNotFound} alt={`${trail.name} trailhead`}/>
+                    <h2>{trail.name}</h2>
+                    {/* url to be used: {https://www.google.com/maps/embed/v1/place?key=AIzaSyCtvdMdqe2ppwzO7Y6faMVpDo-sJG0SkkQ&q=location=${trail.latitude},${trail.longitude}} */}
+                    <Iframe url={`http://google.com`}
+                    width="200px"
+                    height="200px"
+                    id="myId"
+                    className="myClassname"
+                    display="initial"
+                    position="relative"/>
+                <p>
+                    <b><u>Miles:</u></b> {trail.length} 
+                    <br/><br/>
+                    <b><u>Difficulty:</u></b> {trail.difficulty} 
+                    <br/><br/><b><u>Rating:</u></b> {trail.stars} out of {trail.starVotes} votes
+                    <br/><br/><b><u>Latest Conditions:</u></b> {trail.conditionStatus} {trail.conditionDetails}
+                    <br/><br/>
+                </p>
+                </div>
+                )}
+            </div>
+          )
+
+        }
 
     }
     
