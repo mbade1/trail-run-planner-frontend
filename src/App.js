77c6ac71fc6afef7e8cdb996643423cd7296346a
mbade1 <mbade314@gmail.com>
2020-10-22 12:16:20 -0500

    Store disconnected from App.js (not needed)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
-import { connect } from 'react-redux';
 import NavBar from './components/NavBar';
-import Reducer from './reducers/reducer';
 import Footer from './components/Footer'
 
 class App extends React.Component {
@@ -17,24 +15,5 @@ class App extends React.Component {
   }
 }
 
-//used to see what is IN our store.
-// const mapStateToProps = (state) => {
-//   return {
-//     user: state.user,
-//     trails: state.trails,
-//     journals: state.journals
-//   }
-// }
-
-// function mapDispatchToProps(dispatch){
-//   return {     
-//     signUpFetch: () => dispatch(Reducer()),
-//     // fetchTrails: () => dispatch(Reducer()),
-//     // journalFetch: () => dispatch(Reducer()),
-//     // logInFetch: () => dispatch(Reducer()),
-//     // logOutFetch: () => dispatch(Reducer())
-//   }
-// }
-
 //below can also take mapDispatchToProps. Passing in the mapStateToProps func. The return val of that func is coming from our store. 
-export default (App);
+export default App;
